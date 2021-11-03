8513f249f94d5e7d620bd3291018b5fb9c5569b3
sancher6 <sancher6@tcnj.edu>
2021-11-03 19:51:58 -0400

    Working Queue and Play Song

--- groovier/__init__.py
+++ groovier/__init__.py
@@ -1,4 +1,2 @@
 from .main import bot
-from .keep_alive import keep_alive
 from .utils import concat_args
-from .background_tasks import
--- groovier/main.py
+++ groovier/main.py
@@ -10,19 +10,17 @@ import urllib.parse
 import re
 import pafy
 import asyncio
-
-
-# from keep_alive import keep_alive
+from itertools import cycle
 import youtube_dl
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-# keep_alive()
 THUMBS_UP = "\N{THUMBS UP SIGN}"
 THUMBS_DOWN = "\N{THUMBS DOWN SIGN}"
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
 bot = commands.Bot(command_prefix="-")
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+song_queue=[]
 
 
 @bot.event
@@ -58,57 +56,14 @@ async def leave(ctx):
     if voice is not None:
         await voice.disconnect()
 
-
-@bot.command(name="test", help="Test Sample MP3 like this: !test")
-async def test_mp3(ctx, *args):
-    file_name = Path(concat_args(args) + ".mp3")
-    sounds_dir = Path("../test/sounds")
-    test_file = sounds_dir / file_name
-
-    if not Path(test_file).exists():
-        logging.info("File Path Does not Exist")
-        return
-
-    author_connected = ctx.author.voice
+async def wait_queue(ctx): 
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-    if author_connected:
-
-        if (voice is None) or (voice.channel is not author_connected.channel):
-
-            voice = await author_connected.channel.connect()
-            logging.info(f"We have succesfully joined: {author_connected.channel}")
-
-        voice.play(discord.FFmpegPCMAudio(test_file))
-        # await voice.disconnect()
-
-    else:
-        await ctx.send("Please join a voice chat prior to using this command")
-
-
-@bot.command(name="url", help="Test Sample URL to mp3 like this: !url")
-async def test_url(ctx, *args):
-    search = concat_args(args)
-    search = urllib.parse.quote(search)
-    logging.info(f"Search: {search}")
-    html = urllib.request.urlopen(
-        f"https://www.youtube.com/results?search_query={search}"
-    )
-    logging.info(f"the fucker is this: {html}")
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    video_url = "https://www.youtube.com/watch?v=" + video_ids[0]
-    ydl_opts = {
-        "format": "bestaudio/best",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([video_url])
+    while len(song_queue) > 0: 
+        if not voice.is_playing(): 
+            await ctx.send("Now Playing...")
+            voice.play(song_queue[0])
+            del song_queue[0]
+        await asyncio.sleep(1)
 
 
 @bot.command(name="play", aliases=['p'], help="Play song like this: !p")
@@ -125,62 +80,74 @@ async def play_song(ctx, *args):
 
             voice = await author_connected.channel.connect()
             logging.info(f"We have succesfully joined: {author_connected.channel}")
-        search = concat_args(args)
-        search = urllib.parse.quote(search)
+        if len(args) > 0: 
+            search = concat_args(args)
+            search = urllib.parse.quote(search)
 
-        html = urllib.request.urlopen(
-            f"https://www.youtube.com/results?search_query={search}"
-        )
-        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-        if len(video_ids) == 0: 
-            await ctx.send("No results found...")
-        song_url = f"https://www.youtube.com/watch?v={video_ids[0]}"
-        logging.info(f"Song Url: {song_url}")
+            html = urllib.request.urlopen(
+                f"https://www.youtube.com/results?search_query={search}"
+            )
+            video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+            if len(video_ids) == 0: 
+                await ctx.send("No results found...")
+                return
+            song_url = f"https://www.youtube.com/watch?v={video_ids[0]}"
+            logging.info(f"Song Url: {song_url}")
 
-        song = pafy.new(video_ids[0])
+            song = pafy.new(video_ids[0])
+
+            audio = song.getbestaudio()
+
+            source = discord.FFmpegPCMAudio(audio.url, **FFMPEG_OPTIONS)
+            if not voice.is_playing(): 
+                await ctx.send("Now Playing...")
+                voice.play(source)
+            else: 
+                song_queue.append(source)
+                await ctx.send("Song Queued")
+                asyncio.run_coroutine_threadsafe(wait_queue(ctx), bot.loop)
+
+
+@bot.command(name="stop", aliases=['s'], help="Stop song like this: !s")
+async def stop_playing(ctx, *args):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing(): 
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.command(name="next", aliases=['n'], help="Play next song like this: -n")
+async def next_song(ctx):
+    voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+    voice_client.pause()
+    del song_queue[0]
+    if len(song_queue) > 0: 
+        song = pafy.new(song_queue[0])
 
         audio = song.getbestaudio()
 
         source = discord.FFmpegPCMAudio(audio.url, **FFMPEG_OPTIONS)
-
-        try: 
-            voice.play(source)
-        except discord.errors.ClientException: 
-            # We are already playing Music in a channel
-            if voice.channel is not author_connected.channel: 
-                await ctx.send("Groovier is already Running in another Voice Channel")
-                return
-            # Queue the song 
-            with open("queue.txt", "a") as f: 
-                f.write(video_ids[0])
-                f.write("\n")
-            return
+        voice_client.play(source)
+        del song_queue[0]  
 
 
-async def wait_queue(): 
-    await bot.wait_until_ready()
-    while not bot.is_closed: 
-        with open("queue.txt", "w") as f: 
-            lines = f.readlines() 
-            if len(lines) > 0: 
-                logging.info("Playing Next Song...")
-                voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-                next_song = lines[0].rstrip()
-                del lines[0]
-                f.write(lines)
-                song = pafy.new(next_song)
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+        
 
-                audio = song.getbestaudio()
-
-                source = discord.FFmpegPCMAudio(audio.url, **FFMPEG_OPTIONS)
-
-                try: 
-                    voice.play(source)
-                except discord.errors.ClientException:
-                    pass 
-        await asyncio.sleep(1)
-            
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play command")
 
 
-bot.loop.create_task(wait_queue())
 bot.run(TOKEN)
